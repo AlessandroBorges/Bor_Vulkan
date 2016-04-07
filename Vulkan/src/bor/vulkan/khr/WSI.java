8770ca68a28e4a515118a2956350df02f4cc064e
ABorges <AlessandroBorges@gmail.com>
2016-04-07 19:34:19 -0300

    New WSI class

---
+++ Vulkan/src/bor/vulkan/khr/WSI.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package bor.vulkan.khr;
+
+/**
+ * Class to hold functions related to Vulkan's<br>
+ * <b>Window System Integration (WSI) </b>
+ * @author Alessandro Borges
+ *
+ */
+public class WSI {
+
+    /**
+     * 
+     */
+    public WSI() {
+    }
+
+}
