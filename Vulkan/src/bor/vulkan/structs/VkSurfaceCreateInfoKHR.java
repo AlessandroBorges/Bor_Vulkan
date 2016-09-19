df36936130441e8ef86af9a24ca07b515b2be2d3
ABorges <AlessandroBorges@gmail.com>
2016-09-18 22:05:34 -0300

    Interface for all VkXXXSurfaceCreateInfoKHR structures.

---
+++ Vulkan/src/bor/vulkan/structs/VkSurfaceCreateInfoKHR.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bor.vulkan.structs;
+
+/**
+ * Interface for structs:
+ *  {@link VkAndroidSurfaceCreateInfoKHR} , 
+ *  {@link VkWaylandSurfaceCreateInfoKHR}, 
+ *  {@link VkWin32SurfaceCreateInfoKHR},
+ *  {@link VkXlibSurfaceCreateInfoKHR},
+ *  {@link VkXcbSurfaceCreateInfoKHR},
+ *  
+ *  @see VkAndroidSurfaceCreateInfoKHR
+ *  @see VkWaylandSurfaceCreateInfoKHR
+ *  @see VkWin32SurfaceCreateInfoKHR
+ *  @see VkXlibSurfaceCreateInfoKHR
+ *  @see VkXcbSurfaceCreateInfoKHR
+ *  
+ * @author Alessandro Borges
+ */
+public interface VkSurfaceCreateInfoKHR {
+    
+}
